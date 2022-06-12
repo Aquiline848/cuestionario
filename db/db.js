b74f1e4a7b1bcba79c84b0a07b39db94c41f22a6
Mario Rivas Reinoso <99510900+Aquiline848@users.noreply.github.com>
2022-06-12 19:13:13 +0200

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb://mongo:PvsL8bIosivTxkkNxNVJ@containers-us-west-64.railway.app:5938/",
+      procces.env.MONGO_URL,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
